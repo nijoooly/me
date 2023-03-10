0f4eca85f7dbd27cd466528556b2c9fc936ee052
konoplitskii <s.konoplitsky@yandex.com>
2023-03-10 10:15:02 +0300

    Initial commit

---
+++ precache-manifest.f7b8f94feb5abdb3d310b6a96b2edf5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e84a5ae8f71ede771ee",
+    "url": "/me/static/css/main.e7130f48.chunk.css"
+  },
+  {
+    "revision": "3e84a5ae8f71ede771ee",
+    "url": "/me/static/js/main.3e84a5ae.chunk.js"
+  },
+  {
+    "revision": "4910d83cfa244f22b623",
+    "url": "/me/static/js/1.4910d83c.chunk.js"
+  },
+  {
+    "revision": "f4ae667e0f9864d95541",
+    "url": "/me/static/js/runtime~main.f4ae667e.js"
+  },
+  {
+    "revision": "419ff8d4b214c966d1bde05920e23654",
+    "url": "/me/index.html"
+  }
+];
